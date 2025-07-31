73023524562be098b35d192593c0baa5bd406ec4
Carlos Mantilla <carlosmcot@gmail.com>
2025-07-31 15:29:57 -0500

    Test: Update ChannelUITests

--- WalkieTalkieUITests/ChannelUITests.swift
+++ WalkieTalkieUITests/ChannelUITests.swift
@@ -22,13 +22,12 @@ final class ChannelUITests: XCTestCase {
         loginPasswordSecureTextField.typeText("123456")
         
         app/*@START_MENU_TOKEN@*/.buttons["loginEnter"]/*[[".buttons.containing(.staticText, identifier: \"Entrar\").firstMatch",".otherElements",".buttons[\"Entrar\"]",".buttons[\"loginEnter\"]"],[[[-1,3],[-1,2],[-1,1,1],[-1,0]],[[-1,3],[-1,2]]],[0]]@END_MENU_TOKEN@*/.tap()
-
-        app/*@START_MENU_TOKEN@*/.buttons["go down"]/*[[".scrollViews.buttons.firstMatch",".otherElements.buttons[\"go down\"]",".buttons[\"go down\"]"],[[[-1,2],[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        
+        app/*@START_MENU_TOKEN@*/.staticTexts["Desconectado"]/*[[".otherElements.staticTexts[\"Desconectado\"]",".staticTexts.firstMatch",".staticTexts[\"Desconectado\"]"],[[[-1,2],[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
         app.cells/*@START_MENU_TOKEN@*/.containing(.staticText, identifier: "canal-1").firstMatch/*[[".element(boundBy: 0)",".containing(.staticText, identifier: \"canal-1\").firstMatch"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
         app/*@START_MENU_TOKEN@*/.buttons["microphone off"].press(forDuration: 0.9);/*[[".otherElements.buttons[\"microphone off\"]",".tap()",".press(forDuration: 0.9);",".buttons[\"microphone off\"]"],[[[-1,3,1],[-1,0,1]],[[-1,2],[-1,1]]],[0,0]]@END_MENU_TOKEN@*/
         app/*@START_MENU_TOKEN@*/.buttons["power"]/*[[".otherElements.buttons[\"power\"]",".buttons[\"power\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
         app/*@START_MENU_TOKEN@*/.buttons["Cerrar sesión"]/*[[".navigationBars",".buttons.firstMatch",".buttons[\"Cerrar sesión\"]"],[[[-1,2],[-1,0,1]],[[-1,2],[-1,1]]],[0]]@END_MENU_TOKEN@*/.tap()
         app.otherElements.matching(identifier: "Horizontal scroll bar, 1 page").element(boundBy: 1).tap()
-        
     }
 }
